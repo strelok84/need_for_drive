038b89e2d9092b4381476cc968bb6ae75bfcf696
Andrey Bespalov <streloc84@gmail.com>
2021-06-06 16:58:08 +0400

    react-persist

--- src/index.js
+++ src/index.js
@@ -9,10 +9,22 @@ import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
+import {persistStore, persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <BrowserRouter basename="/need_for_drive">
